2e6b1b6f559f898e8eb853cb937e928c28c61df4
2Kelvin <rocketmancodes02@gmail.com>
2024-01-16 20:12:27 +0300

    add the navbar basics

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import './Navbar.css';
+
+export default function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Packages</li>
+                <li>Services</li>
+                <li>Feedback</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
