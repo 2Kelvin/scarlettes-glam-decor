56bbc9717c78040e6d98ee5c1cf362a1a5aa7a64
2Kelvin <rocketmancodes02@gmail.com>
2024-01-27 07:56:10 +0300

    work on main bg add test image, tweak size and add text

--- src/components/Bigbackground/Bigbg.js
+++ src/components/Bigbackground/Bigbg.js
@@ -2,6 +2,8 @@ import './Bigbg.css';
 
 export default function Bigbg() {
     return (
-        <section className="bigBg"></section>
+        <section className="bigBg">
+            <h2>Event Planning & Decoration Services</h2>
+        </section>
     );
 }
