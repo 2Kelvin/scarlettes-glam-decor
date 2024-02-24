9e7b3fff1e4e56a88ca65c782100c9dc8e4a3d67
2Kelvin <rocketmancodes02@gmail.com>
2024-02-24 23:59:22 +0300

    start fixing main bg

--- src/components/Bigbackground/Bigbg.js
+++ src/components/Bigbackground/Bigbg.js
@@ -1,9 +1,12 @@
 import './Bigbg.css';
-import bgPicture from './../../images/table setups/tableSetup5.jpg'; 
+import bgPicture from './../../images/table setups/tableSetup5.jpg';
 
 export default function Bigbg() {
     return (
         <section className="bigBg">
+            <div>
+                <img src={bgPicture} alt='main background' loading='lazy' />
+            </div>
             <h2>Event Planning & Decoration Services</h2>
         </section>
     );
