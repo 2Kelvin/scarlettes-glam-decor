c59a1520260784ba373124a5d1d4d36c70a2f77f
2Kelvin <rocketmancodes02@gmail.com>
2024-02-25 01:20:11 +0300

    replace social icon images with react icons

--- src/components/SocialsIcons/Socials.js
+++ src/components/SocialsIcons/Socials.js
@@ -1,20 +1,18 @@
 import './Socials.css';
-import insta from './../../images/icons/instagram.png';
-import fb from './../../images/icons/fb.png';
-import tiktok from './../../images/icons/tiktok.png';
+import { FaFacebookSquare, FaTiktok, FaInstagramSquare } from "react-icons/fa";
 
 
 export default function Socials() {
     return (
         <div className='mediaIconsDiv'>
             <a href='https://www.facebook.com/profile.php?id=100068952905326&mibextid=2JQ9oc' target='blank'>
-                <img loading='lazy' src={fb} alt='Facebook' className='iconMedia' />
+                <FaFacebookSquare className='iconMedia fbIcon' />
             </a>
             <a href='https://www.tiktok.com/@scarlettesglamdecor?_t=8jkS6FDi7ef&_r=1' target='blank'>
-                <img loading='lazy' src={tiktok} alt='Tiktok' className='iconMedia' />
+                <FaTiktok className='iconMedia tiktokIcon' />
             </a>
             <a href='https://instagram.com/scarlettesglam?igshid=NTA5ZTk1NTc=' target='blank'>
-                <img loading='lazy' src={insta} alt='Instagram' className='iconMedia' />
+                <FaInstagramSquare className='iconMedia instagramIcon' />
             </a>
         </div>
     );
