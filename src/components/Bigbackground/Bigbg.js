d3435a20561fe67d0b3bc7026c1e43bed8dfd64d
2Kelvin <rocketmancodes02@gmail.com>
2024-03-01 00:02:04 +0300

    add blur load animation

--- src/components/Bigbackground/Bigbg.js
+++ src/components/Bigbackground/Bigbg.js
@@ -1,11 +1,25 @@
 import './Bigbg.css';
 import bgPicture from './../../images/picnics/picnic3.jpg';
+import smallBlurBg from '../../images/small-pics/picnic3-small.jpg';
+import { useState } from 'react';
 
 export default function Bigbg() {
+    const [imgLoaded, setImageLoaded] = useState(false);
+
     return (
         <section className="bigBg">
-            <div>
-                <img src={bgPicture} alt='main background' loading='lazy' />
+            <div
+                className={imgLoaded ? 'blurredImgDiv loadedImg' : 'blurredImgDiv'}
+                style={{ backgroundImage: `url(${smallBlurBg})` }}
+            >
+                <img
+                    src={bgPicture}
+                    alt='main background'
+                    loading='lazy'
+                    // if image is loaded add the 'loadedImg' class to its parent div
+                    // which will make the image visible by updating opacity to 1
+                    onLoad={() => setImageLoaded(true)}
+                />
             </div>
             <h2>Event Planning & Decoration Services</h2>
         </section>
